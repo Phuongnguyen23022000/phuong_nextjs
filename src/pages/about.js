42c6e787d665944a988198358177a8f7102a376c
Nguuyenvanphuong <Nguyenvanphuong.23032000@gmail.com>
2023-03-07 14:10:33 +0700

    config

---
+++ src/pages/about.js
@@ -0,0 +1,5 @@
+export default function About() {
+  return <>
+    <h1>Hello My name is Phuong</h1>
+  </>
+}
---
+++ src/pages/profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const profile = () => {
+  return (
+    <div>
+      <h3 className='text-3xl'>Xin cheo</h3>
+    </div>
+  );
+};
+
+export default profile;
